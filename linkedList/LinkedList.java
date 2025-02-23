e000d4118469d47a43d435d011890466cf58960d
virajpwdn <virajpatwardhan6@gmail.com>
2025-02-23 23:53:56 +0530

    print linked list

--- linkedList/LinkedList.java
+++ linkedList/LinkedList.java
@@ -31,8 +31,9 @@ public class LinkedList {
     public void addLast(int data) {
         // 1. Create new Node
         Node newNode = new Node(data);
-        if(head == null && tail == null){
-            
+        if (head == null) {
+            head = tail = newNode;
+            return;
         }
 
         // 2. update tail.next from null to newNode
@@ -42,9 +43,29 @@ public class LinkedList {
         tail = newNode;
     }
 
+    public void print() {
+        Node temp = head;
+        if (temp == null) {
+            System.out.println("LL is empty");
+            return;
+        }
+        while (temp != null) {
+            System.out.print(temp.data + "->");
+            temp = temp.next;
+        }
+        System.out.println("null");
+    }
+
     public static void main(String[] args) {
         LinkedList ll = new LinkedList();
+        ll.print();
         ll.addFirst(1);
+        ll.print();
         ll.addFirst(2);
+        ll.print();
+        ll.addLast(3);
+        ll.print();
+        ll.addLast(4);
+        ll.print();
     }
 }
