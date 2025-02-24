68c3a1c241f02f8807fe5ec4e827134819245cfb
virajpwdn <virajpatwardhan6@gmail.com>
2025-02-24 13:41:12 +0530

    linkedlist remove nodes

--- linkedList/LinkedList.java
+++ linkedList/LinkedList.java
@@ -13,10 +13,12 @@ public class LinkedList {
 
     public static Node head;
     public static Node tail;
+    public int size;
 
     public void addFirst(int data) {
         // 1. Create New Node
         Node newNode = new Node(data);
+        size++;
         if (head == null) {
             head = tail = newNode;
             return;
@@ -31,6 +33,7 @@ public class LinkedList {
     public void addLast(int data) {
         // 1. Create new Node
         Node newNode = new Node(data);
+        size++;
         if (head == null) {
             head = tail = newNode;
             return;
@@ -56,16 +59,50 @@ public class LinkedList {
         System.out.println("null");
     }
 
+    public void addInMiddle(int idx, int data) {
+        if (idx == 0) {
+            addFirst(data);
+            return;
+        }
+        Node newNode = new Node(data);
+        size++;
+        Node temp = head;
+        int i = 0;
+        while (i < idx - 1) {
+            i++;
+            temp = temp.next;
+        }
+        newNode.next = temp.next;
+        temp.next = newNode;
+    }
+
+    public int removeFirst(){
+        if(size == 0){
+            System.out.println("LinkedList is empyt!");
+            return -1;
+        } else if(size == 1){
+            int val = head.data;
+            head = tail = null;
+            size = 0;
+            return val;
+        }
+
+        int val = head.data;
+        head = head.next;
+        size--;
+        return val;
+    }
+
     public static void main(String[] args) {
         LinkedList ll = new LinkedList();
-        ll.print();
         ll.addFirst(1);
-        ll.print();
         ll.addFirst(2);
-        ll.print();
         ll.addLast(3);
-        ll.print();
         ll.addLast(4);
+        ll.addInMiddle(2, 9);
+
+        ll.removeFirst();
         ll.print();
+        System.out.println(ll.size);
     }
 }
