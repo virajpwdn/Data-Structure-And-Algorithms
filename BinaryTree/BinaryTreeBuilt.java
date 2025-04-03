bc29a4e4d79dee6515eed5465d741980e5ac7827
virajpwdn <virajpatwardhan6@gmail.com>
2025-04-03 17:21:07 +0530

    Starting with Binary Trees

---
+++ BinaryTree/BinaryTreeBuilt.java
@@ -0,0 +1,22 @@
+public class BinaryTreeBuilt {
+
+    class Node {
+        int val;
+        Node left = null;
+        Node right = null;
+
+        Node(int val) {
+            this.val = val;
+        }
+    }
+
+    Node root;
+    // public Node createTree(int data){
+    //     if(data == 1) return null;
+    //     Node newNode = Node(data);
+    // }
+
+    public static void main(String[] args) {
+        // Tree newNode = new Tree()
+    }
+}
