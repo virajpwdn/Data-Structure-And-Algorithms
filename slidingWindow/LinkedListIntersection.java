7856c15f52510072c02206ab1f39940a49d78c3d
virajpwdn <virajpatwardhan6@gmail.com>
2026-02-15 07:54:29 +0530

    lc 160 intersection two linkedlist optimised

---
+++ slidingWindow/LinkedListIntersection.java
@@ -0,0 +1,75 @@
+public class Solution {
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        if (headA == null || headB == null)
+            return null;
+        // HashSet<ListNode> set = new HashSet<ListNode>();
+
+        // ListNode tempHeadA = headA;
+        // ListNode tempHeadB = headB;
+
+        // while(tempHeadA != null) {
+        //     set.add(tempHeadA);
+        //     tempHeadA = tempHeadA.next;
+        // }
+
+        // while(tempHeadB != null) {
+        //     if(set.contains(tempHeadB)) {
+        //         return tempHeadB;
+        //     }
+
+        //     set.add(tempHeadB);
+        //     tempHeadB = tempHeadB.next;
+        // }
+
+        // return null;
+
+        int p1 = 0, p2 = 0;
+        ListNode curr = headA;
+        while (curr != null) {
+            p1++;
+            curr = curr.next;
+        }
+
+        curr = headB;
+        while (curr != null) {
+            p2++;
+            curr = curr.next;
+        }
+
+        int diff = 0;
+        if (p1 > p2) {
+            diff = p1 - p2;
+            curr = headA;
+            while (diff != 0) {
+                diff--;
+                curr = curr.next;
+            }
+            ListNode temp = headB;
+            while (curr != null) {
+                if (curr == temp) {
+                    return temp;
+                }
+
+                curr = curr.next;
+                temp = temp.next;
+            }
+        } else {
+            diff = p2 - p1;
+            curr = headB;
+            while (diff != 0) {
+                diff--;
+                curr = curr.next;
+            }
+            ListNode temp = headA;
+            while (curr != null) {
+                if (curr == temp) {
+                    return temp;
+                }
+
+                curr = curr.next;
+                temp = temp.next;
+            }
+        }
+        return null;
+    }
+}
